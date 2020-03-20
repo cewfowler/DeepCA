7ef13a4b54609acddc99480f6297646a32b1979c
cewfowler <cewfowler@gmail.com>
2020-03-20 10:29:54 -0400

    Add vars and simple 2-layer LSTM implementation
    Add global vars
    
    Create tf variables
    
    Change LSTM to 2-layer

--- DeepCA_LSTM.py
+++ DeepCA_LSTM.py
@@ -4,7 +4,38 @@ import sys
 import json
 import numpy as np
 import tensorflow as tf
-import tensorflow.compat.v1 as tfv1
+from tensorflow.contrib import rnn
+
+n_input = 3;
+learning_rate = 0.001;
+training_steps = 10000;
+batch_size = 128;
+n_hidden = 512;
+n_output = 5;
+
+weights = {
+    'out': tf.Variable(tf.random_normal([n_hidden, n_output]));
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([n_output]))
+}
+
+
+def RNN(x, weights, biases):
+    # reshape to [1, n_input]
+    x = tf.reshape(x, [-1, n_input]);
+
+    # Generate a n_input-element sequence of inputs
+    x = tf.split(x, n_input, 1);
+
+    # 2-layer LSTM with n_hidden units
+    rnn_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(n_hidden), rnn.BasicLSTMCell(n_hidden)]);
+
+    # generate prediction
+    outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32);
+
+    # there are n_input outputs but we only want the last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out'];
 
 
 def main():
