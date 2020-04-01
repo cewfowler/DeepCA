241496dcf83301403182216b95d88ae984e8ae7a
cewfowler <cewfowler@gmail.com>
2020-04-01 14:20:30 -0400

    Change model implementation to keras

--- DeepCA_GRU.py
+++ DeepCA_GRU.py
@@ -4,18 +4,26 @@
 
 import os;
 import sys;
-
 import json;
 import numpy as np;
 import tensorflow as tf;
-import tensorflow.compat.v1.nn as tfv1;
+
+from keras.models import Sequential;
+from keras.layers import Dense, GRU, Reshape;
+
+"""
+import tensorflow.compat.v1 as tf;
+
+from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.compat.v1.keras.backend import set_session
+"""
 
 n_input = 3;
 
 relu_clip = 20;
 
 # Number of connections at each layer
-n_hidden = {2048, 2048, 2048, 512, 512};
+n_hidden = [2048, 2048, 2048, 512, 512, 2048, 256];
 
 batch_size = 128;
 learning_rate = 0.001;
@@ -25,68 +33,50 @@ n_epochs = 100;
 
 n_output = 5;
 
-"""
-weights = {
-    'out': tf.Variable(tf.zeros([n_hidden, n_output]))
-}
-biases = {
-    'out': tf.Variable(tf.zeros([n_output]))
-}
-"""
 
-# DeepSpeech implementation referenced
-# Dense layers with relu activation
-# h(t, l) = g( W(l) * h(t, l-1) + b(l) ), W = weights matrix, b = bias matrix,
-#   g(z) = min{ max{0,z}, relu_clip } is clipped recitified-linear (ReLu)
-#          activation function
-def dense(name, x, units, dropout_rate=None):
-    with tfv1.variable_scope(name):
-        bias = tfv1.get_variable(name='bias', \
-                                shape=[units], \
-                                initializer=tfv1.zeros_initializer());
-        weights = tfv1.get_variable(name='weights', \
-                                    shape=[x.shape[-1], units], \
-                                    initializer=tfv1.keras.initializers.VarianceScaling(scale=1.0, mode="fan_avg", distribution="uniform"));
+def create_model():
+    """
+    tf.keras.backend.clear_session();
+    config_proto = tf.ConfigProto()
+    off = rewriter_config_pb2.RewriterConfig.OFF
+    config_proto.graph_options.rewrite_options.arithmetic_optimization = off
+    session = tf.Session(config=config_proto)
+    set_session(session)
+    """
 
-        output = tf.nn.bias_add(tf.matmul(x, weights), bias);
+    x = np.ones((2048, 360, 3));
+    y = np.ones((2048, 256));
 
-        output = tf.minimum(tf.nn.relu(output), relu_clip);
+    model = Sequential();
 
+    # Add dense layers (first 3 layers)
+    model.add(Dense(n_hidden[0], activation='relu'));
+    model.add(Dense(n_hidden[1], activation='relu'));
+    model.add(Dense(n_hidden[2], activation='relu'));
 
-# GRU layers
-def gru_impl(name, x, units, seq_length, prev_state, reuse):
-    with tfv1.variable_scope(name):
-        # GRU cell with n_hidden units
-        gru_cell = tfv1.rnn_cell.GRUCell(units, \
-                                        forget_bias=0, \
-                                        reuse=reuse)
+    model.add(Reshape((-1, n_hidden[3])));
 
-    # generate prediction
-    outputs, states = gru_cell(inputs=x, \
-                                dtype=tf.float32, \
-                                sequence_length=seq_length, \
-                                initial_state=prev_state);
+    # Add recurrent GRU layers (layers 4 and 5)
+    model.add(GRU(n_hidden[3], activation='tanh', recurrent_activation='sigmoid'));
+    model.add(Reshape((-1, n_hidden[4])));
 
-    return output, output_state;
+    model.add(GRU(n_hidden[4], activation='tanh', recurrent_activation='sigmoid'));
 
-def create_model(batch_x, seq_length, dropout, reuse=False, batch_size=None, prev_state=None):
-    layers = {};
+    # Add another dense layer
+    model.add(Dense(n_hidden[5], input_dim=n_hidden[4], activation='relu'));
 
-    # First 3 layers (Dense with ReLu activation)
-    layers['layer_1'] = layer_1 = dense('layer_1', batch_x, n_hidden[0], dropout_rate=dropout[0]);
-    layers['layer_2'] = layer_2 = dense('layer_2', layer_1, n_hidden[1], dropout_rate=dropout[1]);
-    layers['layer_3'] = layer_3 = dense('layer_3', layer_2, n_hidden[2], dropout_rate=dropout[2]);
+    # Add output layer
+    model.add(Dense(n_hidden[6], input_dim=n_hidden[5], activation='softmax'));
 
-    layer_3 = tf.reshape(layer_3, [-1, batch_size, n_hidden[3]])
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']);
 
-    # Next 2 layers (GRU)
-    output_1, out_state_1 = gru_impl('layer_4', layer_3, n_hidden[3], seq_length, prev_state[0], reuse);
-    layers['layer_4'] = output_1;
-    layers['gru_out_state_1'] = out_state_1;
+    model_json = model.to_json();
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json);
+    model.fit(x,y)
 
-    output_2, out_state_2 = gru_impl('layer_5', output_1, n_hidden[4], seq_length, prev_state[1], reuse);
-    layers['layer_5'] = output_2;
-    layers['gru_out_state_2'] = out_state_2;
+    model.save_weights("model.h5");
 
 
 def main():
